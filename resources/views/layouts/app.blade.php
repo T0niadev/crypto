6ff202462408172668809c6c2da9c27e1e6467f3
toniadev <odubiyiifeolu@gmail.com>
2022-09-20 18:01:43 +0100

    remaning admin css, navbar and payment integration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,8 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
   
+  <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.3/dist/flowbite.min.css" />
+  <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
 
 
   <!-- CSS Assets -->
--- resources/views/user/Wallet/index.blade.php
+++ resources/views/user/Wallet/index.blade.php
@@ -5,6 +5,27 @@
   <div class="mt-4">
    <p class="text-2xl font-semibold">Welcome {{auth()->user()->username}}</p>
   </div>
+
+
+
+<div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full justify-center items-center" aria-hidden="true">
+    <div class="relative p-4 w-full max-w-md h-full md:h-auto">
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="popup-modal">
+                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+            <div class="p-6 text-center">
+                <svg aria-hidden="true" class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                <h3 class="text-lg font-normal text-gray-500 dark:text-gray-400">Sorry, this payment method in currently unavailable in your country.</h3>
+                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Contact us via email: support@cryptfxtrading.com for support and enquiries.</h3>
+         
+                <button data-modal-toggle="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Back</button>
+            </div>
+        </div>
+    </div>
+</div>
+
     <div class="mt-4 grid grid-cols-12 gap-4 sm:mt-5 sm:gap-5 lg:mt-6 lg:gap-6">
       <div class="col-span-12 grid grid-cols-12 rounded-lg bg-gradient-to-r from-blue-500 to-indigo-600 py-5 sm:py-6">
         <div class="col-span-12 sm:col-span-6 lg:col-span-4">
@@ -144,9 +165,7 @@
                                 <p class="mt-1 text-xs font-medium">
                                 Make Deposit With
                                 </p>
-                                <a  href="" class="text-lg font-semibold tracking-wide">
-                                    Paypal
-                                </a>
+                               
 
                             </div>
                         </div>
@@ -166,9 +185,9 @@
                     <p class="mt-1 text-xs font-medium">
                       Make Deposit With
                     </p>
-                    <a href="" class="text-lg font-semibold tracking-wide">
-                      Paypal
-                    </a>
+                    <a class='cursor-pointer' data-modal-toggle="popup-modal">
+  PayPal
+              </a>
 
 
                     <p class="mt-1 text-xs font-medium">
@@ -188,11 +207,12 @@
                         d="M5.343 7.257a.5.5 0 01.354.147 6.5 6.5 0 010 9.192.64.64 0 00-.073.087 4.718 4.718 0 01-.56-.094 1.301 1.301 0 01-.109-.03.5.5 0 01.035-.67 5.5 5.5 0 000-7.778.5.5 0 01.353-.854zm-.422 9.288a.06.06 0 010 0zM15 1.6a.5.5 0 01.354.147 14.5 14.5 0 010 20.506.5.5 0 11-.708-.707 13.5 13.5 0 000-19.092A.5.5 0 0115 1.6zM10.172 4.43a.5.5 0 01.353.146 10.5 10.5 0 010 14.85.5.5 0 11-.707-.707 9.5 9.5 0 000-13.436.5.5 0 01.354-.853z" />
                     </svg>
                   </div>
-                  <p class="mt-1 text-xs font-medium">
-                    Make Deposit With
-                  </p>
-                  <div class="text-white">
-                    <a href="" class="text-lg font-semibold tracking-wide">
+                 
+                    <p class="mt-1 text-white font-medium">
+                      Make Deposit With
+                    </p>
+                    <div class="text-white">
+                    <a class='cursor-pointer' data-modal-toggle="popup-modal">
                       Stripe
                     </a>
 
